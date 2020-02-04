83b30e1b02320c2268be93552900a6c066ee07c4
Squ-123 <34301305+Squ-123@users.noreply.github.com>
2020-02-04 21:22:15 +0800

    transformer

---
+++ Transformer/batch.py
@@ -0,0 +1,22 @@
+#输出batch数据集
+#参考内容
+import numpy as np
+
+def nextBatch(x, y, batchSize):
+    """
+    生成batch数据集，用生成器的方式输出
+    """
+    perm = np.arange(len(x))
+    np.random.shuffle(perm)
+    x = x[perm]
+    y = y[perm]
+
+    numBatches = len(x) // batchSize
+
+    for i in range(numBatches):
+        start = i * batchSize
+        end = start + batchSize
+        batchX = np.array(x[start: end], dtype="int64")
+        batchY = np.array(y[start: end], dtype="float32")
+
+        yield batchX, batchY
